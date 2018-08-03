1fc33cbf7034a111bdf4e3db7b0833f68e8649f4
Daniel Home <danielmoore.info@gmail.com>
2018-08-03 20:01:14 +1000

    Added some more tools to download

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -12,12 +12,23 @@ chown -R vagrant:vagrant /home/vagrant/.ssh
 
 mkdir /tools
 cd /tools
+# Clone some tools I commonly use
 git clone https://github.com/Raikia/CredNinja.git
 git clone https://github.com/lgandx/PCredz.git
 git clone https://github.com/danielmoore-info/IPCleaner.git
 git clone https://github.com/maurosoria/dirsearch.git
-apt-get install crackmapexec
 
+# Install routersploit
+apt-get -y install python3-pip
+git clone https://www.github.com/threat9/routersploit
+cd routersploit
+python3 -m pip install -r requirements.txt
+
+# Install crackmapexec
+apt-get -y install crackmapexec
+
+# Install tool to interact with iscsi
+apt-get -y install open-iscsi
 
 # Install NFS client
 apt-get -y install nfs-common
